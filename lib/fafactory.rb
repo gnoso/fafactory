c5931a2e097763232d796b187ffbbc972cd40a1c
Alan Johnson <alan@commondream.net>
2009-08-06 11:05:13 -0400

    Got fafactory command to put apps into test mode.

--- lib/fafactory.rb
+++ lib/fafactory.rb
@@ -1,19 +1,26 @@
 require 'active_resource'
 
 class Fafactory < ActiveResource::Base
-  self.site = "http://0.0.0.0:3000/"
+  FAFACTORY_CONFIG = YAML.load('config/fafactory.yml')
   
   # Creates a new instance of a remote model using the data provided.
-  def self.create_instance(model, data)
+  def self.create_instance(service, model, data)
+    Fafactory.configure_site(service)
     result = Fafactory.post :create_instance, nil, 
         { :model => model, :data => data }.to_xml
     
     return Hash.from_xml(result.body)
   end
   
-  def self.purge
+  def self.purge(service)
+    Fafactory.configure_site(service)
     Fafactory.delete(:purge)
     
     nil
+  end
+  
+  private
+  def self.configure_site(service)
+    self.site = "http://0.0.0.0:#{FAFACTORY_CONFIG["service"]["port"]}"
   end
 end
