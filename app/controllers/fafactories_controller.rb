1802caa752e872bcaa8f699f3b5a95696f07bc98
Alan Johnson <alan@commondream.net>
2009-08-06 14:49:27 -0400

    Got test only mode in place and got gem packaged up.

--- app/controllers/fafactories_controller.rb
+++ app/controllers/fafactories_controller.rb
@@ -1,49 +1,51 @@
-class FafactoriesController < ApplicationController
+if Rails.env == "test"
+  class FafactoriesController < ApplicationController
   
-  # POST a new instance of the type of model requested
-  #
-  # ==== Parameters
-  # model<String>:: The model to create a new instance of
-  # data<Hash>:: A hash of the attributes for the model
-  def create_instance
-    obj = Module.const_get(params["hash"]["model"].to_sym).new
+    # POST a new instance of the type of model requested
+    #
+    # ==== Parameters
+    # model<String>:: The model to create a new instance of
+    # data<Hash>:: A hash of the attributes for the model
+    def create_instance
+      obj = Module.const_get(params["hash"]["model"].to_sym).new
     
-    params["hash"]["data"].each do |key, value|
-      obj.send(:"#{key}=", value)
-    end
+      params["hash"]["data"].each do |key, value|
+        obj.send(:"#{key}=", value)
+      end
     
-    obj.save!
+      obj.save!
       
-    respond_to do |format|
-      format.xml do
-        render :xml => obj, :status => :created, :location => obj
+      respond_to do |format|
+        format.xml do
+          render :xml => obj, :status => :created, :location => obj
+        end
       end
     end
-  end
   
-  # Purges the test database
-  def purge
+    # Purges the test database
+    def purge
     
-    # We have to disconnect the connection for active record because the
-    # connection doesn't make it through the fork, and the rake task will
-    # do it's own database load, etc anyway
-    dbconfig = ActiveRecord::Base.remove_connection
-    pid = fork do
+      # We have to disconnect the connection for active record because the
+      # connection doesn't make it through the fork, and the rake task will
+      # do it's own database load, etc anyway
+      dbconfig = ActiveRecord::Base.remove_connection
+      pid = fork do
       
-      require 'rake'
-      require 'rake/testtask'
-      require 'rake/rdoctask'
+        require 'rake'
+        require 'rake/testtask'
+        require 'rake/rdoctask'
 
-      require 'tasks/rails'
+        require 'tasks/rails'
       
       
-      Rake::Task['db:test:load'].invoke
-    end
-    ActiveRecord::Base.establish_connection(dbconfig)
-    Process.wait(pid)
+        Rake::Task['db:test:load'].invoke
+      end
+      ActiveRecord::Base.establish_connection(dbconfig)
+      Process.wait(pid)
     
-    respond_to do |format|
-      format.xml { head :ok }
+      respond_to do |format|
+        format.xml { head :ok }
+      end
     end
   end
 end
---
+++ fafactory.gemspec
@@ -0,0 +1,25 @@
+PKG_VERSION = "0.0.1"
+
+Gem::Specification.new do |s|
+
+  s.name = 'fafactory'
+  s.version = PKG_VERSION
+  s.platform = Gem::Platform::RUBY
+  s.description = <<-DESC.strip.gsub(/\n\s+/, " ")
+    Fafactory (originally Far Away Factory) is a tool for remotely creating instances of ActiveRecord models within a service. This is useful when doing integration tests of services, because it allows you to set up the environment within the remote service from your test, rather than trying to keep an instance of the service in pristine shape.
+  DESC
+  s.summary = <<-DESC.strip.gsub(/\n\s+/, " ")
+    Framework for creating objects in remote services.
+  DESC
+  
+  s.files = Dir.glob("{lib,app,bin}/**/*") | ["config/routes.rb"]
+  s.require_path = 'lib'
+  s.has_rdoc = true
+  
+  s.bindir = "bin"
+  s.executables << "fafactory"
+
+  s.author = "Gnoso, Inc."
+  s.email = "alan@gnoso.com"
+  s.homepage = "http://www.gnoso.com"
+end
